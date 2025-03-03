34374648d98daf4158df13c39141c2d390835f56
Enyewashebr <Enyewashebir0@gmail.com>
2025-03-03 08:48:17 +0300

    finishing the navbar

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,5 +1,9 @@
+
+'use client'
 import Link from "next/link";
 import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -7,15 +11,20 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }>) {
   return (
-   <div className="h-screen flex">
+   <div className="h-100vh flex">
     {/* Left side */}
-    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-gray-200">
-        <Link rel="stylesheet" href="/" />
-        <Image src="logo.png" alt="logo"  width={32} height={32}/>
-        <span>Eagle school</span>
+    <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] ">
+        <Link rel="stylesheet" href="/" className="flex items-center justify-center justistify-start gap-2">
+        <Image src="/logo.png" alt="logo"  width={32} height={32}/>
+        <span className="hidden lg:block">Eagle school</span>
+        </Link>
+        <Menu />
     </div>
     {/* right side */}
-    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] ">right</div>
+    <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+      <Navbar />
+      {children} student
+    </div>
    </div>
   );
 }
